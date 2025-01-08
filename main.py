1686f20a7976e723b164fdd14d3b865c19df83bc
JorgeJola <120377392+JorgeJola@users.noreply.github.com>
2025-01-08 06:10:33 -0500

    drive 13

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ main = Blueprint('main', __name__)
 
 # Configura las credenciales para la API de Google Drive
 SCOPES = ['https://www.googleapis.com/auth/drive']
-SERVICE_ACCOUNT_FILE = '.pelagic-gist-434920-h5-91777317c681.json'
+SERVICE_ACCOUNT_FILE = './pelagic-gist-434920-h5-91777317c681'
 
 credentials = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 drive_service = build('drive', 'v3', credentials=credentials)
